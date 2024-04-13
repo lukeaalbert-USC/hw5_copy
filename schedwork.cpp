6e77db47466da9b847e5d5fb798389b643e9de2a
Luke Albert <lpalbert@usc.edu>
2024-04-13 05:34:30 +0000

    code copied from original hw5 repo

--- schedwork.cpp
+++ schedwork.cpp
@@ -10,6 +10,7 @@
 
 #endif
 
+#include <limits.h>
 #include "schedwork.h"
 
 using namespace std;
@@ -21,7 +22,15 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool scheduleHelper (
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched, map<Worker_T, size_t> workerToDaysWorked,
+    size_t currDay,
+    size_t currWorker,
+    size_t workersScheduled,
+    size_t workerLoopHelp);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -38,8 +47,58 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    if (avail[0].size() < dailyNeed) //if the number of k possible workers is less than the d daily needed, no way to create a valid schedule
+    {
+        return false;
+    }
 
+    map<Worker_T, size_t> workerToDaysWorked; //mapping worker to the number of the days theyve worked
+    for (unsigned long int i = 0; i < avail[0].size(); i++) //for all k workers, add the workers' id and set their days worked to 0. 
+    {
+        workerToDaysWorked[i] = 0; //"worker id" days worked = 0
+    }
 
-
+    for (unsigned long int i = 0; i < avail.size(); i++) //initialize sched
+    {
+        std::vector<Worker_T> day;
+        sched.push_back(day);
+        for (size_t j = 0; j < dailyNeed; j++)
+        {
+            sched[i].push_back(INT_MAX); //all vals will be INT_MAX
+        }
+    }
+    return scheduleHelper(avail, dailyNeed, maxShifts, sched, workerToDaysWorked, 0, 0, 0, 0);
 }
 
+bool scheduleHelper(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, map<Worker_T, size_t> workerToDaysWorked, size_t row, size_t col, size_t workersScheduled, size_t workerLoopHelp) {
+    if (row == avail.size() && col == 0) //if we're on the last day and the last worker
+    {
+        return true;
+    }
+    for (int worker = workerLoopHelp; worker < avail[0].size(); worker++) //for all workers
+    {
+        sched[row][col] = worker;
+        workerToDaysWorked[worker]++;
+        workersScheduled++;
+        if (avail[row][worker] == true && workerToDaysWorked[worker] <= maxShifts) //if the worker can work (is valid)
+        {
+            if (workersScheduled == dailyNeed)
+            { 
+                if (scheduleHelper(avail, dailyNeed, maxShifts, sched, workerToDaysWorked, row + 1, 0, 0, 0))
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                if (scheduleHelper(avail, dailyNeed, maxShifts, sched, workerToDaysWorked, row, col + 1, workersScheduled, worker + 1))
+                {
+                    return true;
+                }
+            }
+        }
+        workerToDaysWorked[worker]--;
+        workersScheduled--;
+    }
+    return false;
+}
--- wordle.cpp
+++ wordle.cpp
@@ -5,6 +5,7 @@
 #include <algorithm> 
 #include <map>
 #include <set>
+#include <regex>
 #endif
 
 #include "wordle.h"
@@ -13,7 +14,15 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+void wordleHelper(std::string partial,
+    std::string floating,
+    const std::set<std::string>& dict,
+    std::set<std::string>& result,
+    unsigned int curr);
 
+int countUnderscores(const std::string& str);
+
+// bool contains(std::string partial, const std::string& floating, unsigned int index);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +31,61 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    std::set<std::string> result;
+    std::string partial = in;
 
+    std::string floatingCopy = floating;
+
+    wordleHelper(partial, floatingCopy, dict, result, 0);
+    return result;
 }
 
+
 // Define any helper functions here
+void wordleHelper(std::string partial,
+    std::string floating,
+    const std::set<std::string>& dict,
+    std::set<std::string>& result,
+    unsigned int curr)
+{
+    if (curr == partial.size())
+    { 
+      if (dict.find(partial) != dict.end() && floating.length() == 0) {
+            result.insert(partial);
+      }
+      return;
+    }
+
+    if (partial[curr] != '-')
+    {
+      wordleHelper(partial, floating, dict, result, curr + 1);
+    }
+
+    else
+    {
+      // Add a floating letter
+      for (int j = 0; j < floating.length(); j++) {
+        partial[curr] = floating[j];
+        std::string substr = floating.substr(0,j) + floating.substr(j+1); // get the substring without letter j
+        // put letter j in the partial
+        wordleHelper(partial, substr, dict, result, curr + 1);
+      }
+      if (countUnderscores(partial) >= floating.length()) // number of blanks > the number of floating letters
+      {
+        for (int i = (int) 'a'; i <= (int) 'z'; i++) //for all letters
+        {
+          partial[curr] = char(i);
+          wordleHelper(partial, floating, dict, result, curr + 1);
+        }
+      }
+    }
+}
+
+
+int countUnderscores(const std::string& str) {
+  int count = 0;
+  for (int k = 0; k < str.length(); k++) {
+    count += (int) (str[k] == '-');
+  }
+  return count;
+}
